6fb4f672c40749244ed245779592a4a4db6745d7
ayesha <ayesha_islam@icloud.com>
2023-03-29 02:04:53 +0500

    -a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div>hello world, from ayesha</div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>hello world, from ayesha</div>
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
-}
+};
 
-export default App;
+export default Login;
