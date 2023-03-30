5833b19118ddaa0b881b237944968bb964b0ea95
ayesha <ayesha_islam@icloud.com>
2023-03-30 23:15:14 +0500

    -a

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
 
-// function App() {
-//   return (
-//     <div>hello world, from ayesha</div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from 'react';
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
-
+function App() {
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>    
+    {/* <div>hello world</div> */}
+    <div><Home/></div>
+    </>
   );
-};
+}
 
-export default Login;
+export default App;
+
+// import React, { useState } from 'react';
+
+// const Login = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Add your login logic here
+//     console.log('Email:', email);
+//     console.log('Password:', password);
+//   };
+
+//   return (
+//     <div className="login">
+//       <form onSubmit={handleSubmit}>
+//         <h1>Login</h1>
+//         <label htmlFor="email">Email:</label>
+//         <input
+//           type="email"
+//           id="email"
+//           name="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           type="password"
+//           id="password"
+//           name="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
---
+++ src/components/componentreadme.txt
@@ -0,0 +1,4 @@
+all things that are common in all 
+screens must be declared as a folder in component
+
+name everything with starting capital letter
