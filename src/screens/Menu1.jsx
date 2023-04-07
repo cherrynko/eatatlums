23e7c9519732b0e567ceefcc24b02380c6f3f669
Zossima-F <95336337+Zossima-F@users.noreply.github.com>
2023-04-07 20:09:39 +0500

    Nothing, just testing - Faraz

--- src/screens/Menu1.jsx
+++ src/screens/Menu1.jsx
@@ -5,20 +5,20 @@ import './Menu1.css'
 
 export default function Menu1() {
     return (
-    <div>
-        <EachEateryNavbar />
-        <div className="search-container">
-        <form>
-            <div className="input-container">
-            <button type="submit" className="search-button">
-                <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png"></img>
-                {/* <i className="fa fa-search"></i> */}
-            </button>
-            <input type="text" placeholder="Search.." name="search" className="search-input" />
+        <div>
+            <EachEateryNavbar />
+            <div className="search-container">
+                <form>
+                    <div className="input-container">
+                        <button type="submit" className="search-button">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png"></img>
+                            {/* <i className="fa fa-search"></i> */}
+                        </button>
+                        <input type="text" placeholder="Search.." name="search" className="search-input" />
+                    </div>
+                </form>
             </div>
-        </form>
         </div>
-    </div>
     )
 }
- 
+
