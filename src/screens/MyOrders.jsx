84230119ab73790329236fdfa3d281556a448140
Zossima-F <95336337+Zossima-F@users.noreply.github.com>
2023-04-08 21:08:58 +0500

    Add Review backend & frontend (in progress)

--- src/screens/MyOrders.jsx
+++ src/screens/MyOrders.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 import './Eateries.css'
 import EateriesNavbar from "../components/EateriesNavbar";
 
@@ -8,7 +8,16 @@ export default function MyOrders() {
         <div>
             <EateriesNavbar />
             <h1>My Orders</h1>
+            <form>
+                <div className="input-container">
+                    <button type="submit" className="search-button">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png"></img>
+                        {/* <i className="fa fa-search"></i> */}
+                    </button>
+                    <input type="text" placeholder="Search.." name="search" className="search-input" />
+                </div>
+            </form>
         </div>
-        
+
     )
 }
--- src/screens/Reviews.jsx
+++ src/screens/Reviews.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import EachEateryNavbar from "../components/EachEateryNavbar";
+import AddReview from '../components/AddReview';
+
 
 export default function Reviews() {
   return (
     <div>
-        <EachEateryNavbar />
-        <h1> Reviews </h1>
+      <EachEateryNavbar />
+      <h1> Reviews </h1>
+      <div className="search-container">
+        <form>
+          <div className="input-container">
+            <button type="submit" className="search-button">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png"></img>
+              {/* <i className="fa fa-search"></i> */}
+            </button>
+            <input type="text" placeholder="Search.." name="search" className="search-input" />
+          </div>
+        </form>
+      </div>
+
+      <AddReview />
+
+
+
+
     </div>
   )
 }
